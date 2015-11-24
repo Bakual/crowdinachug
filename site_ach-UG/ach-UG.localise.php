bf1000dacdb6e6e989a35a6c125918bb25883fb2
Thomas Hunziker <werbemails@bakual.ch>
2015-11-24 11:15:39 +0100

    Reverting change in localise.php

--- site_ach-UG/ach-UG.localise.php
+++ site_ach-UG/ach-UG.localise.php
@@ -1,90 +1,90 @@
-crwdns14567:0crwdne14567:0
-crwdns14568:0crwdne14568:0
- crwdns14569:0crwdne14569:0
- crwdns14570:0crwdne14570:0
- crwdns14571:0crwdne14571:0 crwdns14572:0crwdne14572:0
- crwdns14573:0crwdne14573:0
- crwdns14574:0crwdne14574:0
+<?php
+/**
+ * @package    Joomla.Language
+ *
+ * @copyright  Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
-crwdns14575:0crwdne14575:0
+defined('_JEXEC') or die;
 
-crwdns14568:0crwdne14568:0
- crwdns14577:0crwdne14577:0
- crwdns14570:0crwdne14570:0
- crwdns14579:0crwdne14579:0
- crwdns14574:0crwdne14574:0
-crwdns14581:0crwdne14581:0
-crwdns14582:0crwdne14582:0
-	crwdns14568:0crwdne14568:0
-	 crwdns14584:0crwdne14584:0
-	 crwdns14570:0crwdne14570:0
-	 crwdns14883:0crwdne14883:0
-	 crwdns14570:0crwdne14570:0
-	 crwdns14588:0crwdne14588:0
-	 crwdns14570:0crwdne14570:0
-	 crwdns14590:0crwdne14590:0
-	 crwdns14574:0crwdne14574:0
-	crwdns14592:0crwdne14592:0
-	crwdns14582:0crwdne14582:0
-		crwdns14594:0crwdne14594:0
-		crwdns14582:0crwdne14582:0
-			crwdns14884:0crwdne14884:0
-		crwdns14597:0crwdne14597:0
-		crwdns14598:0crwdne14598:0
-		crwdns14582:0crwdne14582:0
-			crwdns14885:0crwdne14885:0
-		crwdns14597:0crwdne14597:0
-		crwdns14602:0crwdne14602:0
-		crwdns14582:0crwdne14582:0
-			crwdns14886:0crwdne14886:0
-		crwdns14597:0crwdne14597:0
-	crwdns14597:0crwdne14597:0
+/**
+ * ach-UG localise class.
+ *
+ * @since  1.6
+ */
+abstract class Ach_UGLocalise
+{
+	/**
+	 * Returns the potential suffixes for a specific number of items
+	 *
+	 * @param   integer  $count  The number of items.
+	 *
+	 * @return  array  An array of potential suffixes.
+	 *
+	 * @since   1.6
+	 */
+	public static function getPluralSuffixes($count)
+	{
+		if ($count == 0)
+		{
+			return array('0');
+		}
+		elseif ($count == 1)
+		{
+			return array('1');
+		}
+		else
+		{
+			return array('MORE');
+		}
+	}
 
-	crwdns14568:0crwdne14568:0
-	 crwdns14609:0crwdne14609:0
-	 crwdns14570:0crwdne14570:0
-	 crwdns14611:0crwdne14611:0
-	 crwdns14570:0crwdne14570:0
-	 crwdns14590:0crwdne14590:0
-	 crwdns14574:0crwdne14574:0
-	crwdns14615:0crwdne14615:0
-	crwdns14582:0crwdne14582:0
-		crwdns14887:0crwdne14887:0
-	crwdns14597:0crwdne14597:0
+	/**
+	 * Returns the ignored search words
+	 *
+	 * @return  array  An array of ignored search words.
+	 *
+	 * @since   1.6
+	 */
+	public static function getIgnoredSearchWords()
+	{
+		return array('and', 'in', 'on');
+	}
 
-	crwdns14568:0crwdne14568:0
-	 crwdns14624:0crwdne14624:0
-	 crwdns14570:0crwdne14570:0
-	 crwdns14626:0crwdne14626:0
-	 crwdns14570:0crwdne14570:0
-	 crwdns14590:0crwdne14590:0
-	 crwdns14574:0crwdne14574:0
-	crwdns14630:0crwdne14630:0
-	crwdns14582:0crwdne14582:0
-		crwdns14632:0crwdne14632:0
-	crwdns14597:0crwdne14597:0
+	/**
+	 * Returns the lower length limit of search words
+	 *
+	 * @return  integer  The lower length limit of search words.
+	 *
+	 * @since   1.6
+	 */
+	public static function getLowerLimitSearchWord()
+	{
+		return 3;
+	}
 
-	crwdns14568:0crwdne14568:0
-	 crwdns14635:0crwdne14635:0
-	 crwdns14570:0crwdne14570:0
-	 crwdns14637:0crwdne14637:0
-	 crwdns14570:0crwdne14570:0
-	 crwdns14590:0crwdne14590:0
-	 crwdns14574:0crwdne14574:0
-	crwdns14641:0crwdne14641:0
-	crwdns14582:0crwdne14582:0
-		crwdns14643:0crwdne14643:0
-	crwdns14597:0crwdne14597:0
+	/**
+	 * Returns the upper length limit of search words
+	 *
+	 * @return  integer  The upper length limit of search words.
+	 *
+	 * @since   1.6
+	 */
+	public static function getUpperLimitSearchWord()
+	{
+		return 20;
+	}
 
-	crwdns14568:0crwdne14568:0
-	 crwdns14646:0crwdne14646:0
-	 crwdns14570:0crwdne14570:0
-	 crwdns14648:0crwdne14648:0
-	 crwdns14570:0crwdne14570:0
-	 crwdns14590:0crwdne14590:0
-	 crwdns14574:0crwdne14574:0
-	crwdns14652:0crwdne14652:0
-	crwdns14582:0crwdne14582:0
-		crwdns14654:0crwdne14654:0
-	crwdns14597:0crwdne14597:0
-crwdns14597:0crwdne14597:0
+	/**
+	 * Returns the number of chars to display when searching
+	 *
+	 * @return  integer  The number of chars to display when searching.
+	 *
+	 * @since   1.6
+	 */
+	public static function getSearchDisplayedCharactersNumber()
+	{
+		return 200;
+	}
+}
